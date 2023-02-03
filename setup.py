6c534b3690049cdcb6cc5ad0d30f254dfa62c043
Adrian Minde <adrian_minde@live.de>
2023-02-03 12:01:50 +0100

    Add setuptools extras_require

--- setup.py
+++ setup.py
@@ -30,11 +30,17 @@ AUTHOR = 'ISC Konstanz'
 MAINTAINER_EMAIL = 'adrian.minde@isc-konstanz.de'
 URL = 'https://github.com/isc-konstanz/th-e-yield'
 
-INSTALL_REQUIRES = ['numpy >= 1.16',
-                    'pandas >= 0.23',
-                    'pvlib >= 0.9',
-                    'NREL-PySAM >= 4.0',
-                    'th-e-core @ git+https://github.com/isc-konstanz/th-e-core@master >= 0.4.2']
+INSTALL_REQUIRES = [
+    'numpy >= 1.16',
+    'pandas >= 0.23',
+    'pvlib >= 0.9',
+    'NREL-PySAM >= 4.0',
+    'th-e-core @ git+https://github.com/isc-konstanz/th-e-core.git@master'
+]
+
+EXTRAS_REQUIRE = {
+    'eval': ['th-e-data[excel,plot] @ git+https://github.com/isc-konstanz/th-e-data.git@master']
+}
 
 SCRIPTS = ['bin/th-e-yield']
 
@@ -55,5 +61,6 @@ setup(
     author_email=MAINTAINER_EMAIL,
     packages=PACKAGES,
     install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
     scripts=SCRIPTS,
 )
