061e16d4803593d5535cb836030d45447b4fa982
Adrian Minde <adrian_minde@live.de>
2025-01-08 13:36:32 +0100

    Restructure main entry point

--- penguin/__main__.py
+++ penguin/__main__.py
@@ -11,20 +11,18 @@ To learn how to use local resource integration systems, see "penguin --help"
 import os
 from argparse import ArgumentParser, RawTextHelpFormatter
 
-import penguin
-
 os.environ["NUMEXPR_MAX_THREADS"] = str(os.cpu_count())
 
 
-def _get_parser() -> ArgumentParser:
-    from penguin import __version__
+def main() -> None:
+    import penguin
 
     parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s {version}".format(version=__version__))
+    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s {penguin.__version__}")
 
-    return parser
+    with penguin.load(parser=parser) as application:
+        application.main()
 
 
 if __name__ == "__main__":
-    with penguin.load(parser=_get_parser()) as application:
-        application.main()
+    main()
