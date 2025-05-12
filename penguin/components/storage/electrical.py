3a108cab359154912f07b4fdb0c49f17bfaf01c1
Adrian Minde <adrian.minde@mail.de>
2025-05-12 17:19:25 +0200

    Restructure charge mode functions

--- penguin/components/storage/electrical.py
+++ penguin/components/storage/electrical.py
@@ -6,6 +6,9 @@ penguin.components.storage.electrical
 
 """
 
+from collections.abc import Callable
+from typing import Any, Dict
+
 import pandas as pd
 from lori.components import Component, register_component_type
 from lori.core import Configurations, Constant
@@ -19,58 +22,57 @@ class ElectricalEnergyStorage(Component):
 
     CYCLES = Constant(int, "ees_cycles", "EES Cycles")
 
-    MODES = ["peak_shaving", "self_consumption", "self_peak_shaving"]
+    MODES = ["self_consumption", "self_peak_shaving", "peak_shaving"]
 
     capacity: float
     efficiency: float
 
-    charge_power_max: float
-    discharge_power_max: float
+    power_max: float
 
     soc_max: float
     soc_min: float
 
     mode: str
-    mode_parameters: dict
-    mode_function: callable
+    _mode_parameters: Dict[str, Any]
+
+    _predict_charge_power: Callable[[float, float, float], float]
 
     def configure(self, configs: Configurations) -> None:
         super().configure(configs)
         self.capacity = configs.get_float("capacity")
         self.efficiency = configs.get_float("efficiency")
 
-        self.charge_power_max = configs.get_float("charge_power_max") * 1000
-        self.discharge_power_max = configs.get_float("discharge_power_max") * 1000
+        power_max = configs.get_float("power_max")
+        self.power_max = power_max * 1000
 
         self.soc_max = configs.get_float("soc_max", default=100)
         self.soc_min = configs.get_float("soc_min", default=0)
 
-        mode = configs.get("mode", default="peak_shaving")
+        mode = configs.get("mode", default="self_consumption")
         if mode not in ElectricalEnergyStorage.MODES:
             raise ValueError(f"Invalid mode '{mode}' for {self.name}.")
 
+        mode_section = configs.get_section(mode, defaults={})
+
         self.mode = mode
+        self._mode_parameters = {
+            "charge_power_max": mode_section.get_float("charge_power_max", default=power_max) * 1000,
+        }
+        if mode == "self_consumption":
+            self._mode_parameters["grid_power_target"] = mode_section.get_float("grid_power_target", default=0.0) * 1000
+            self._predict_charge_power = self._predict_self_consumption
 
-        mode_config = configs.get_section(mode)
-        self.mode_parameters = {}
-        if mode == "peak_shaving":
-            self.mode_parameters["grid_power_max"] = mode_config.get_float("grid_power_max") * 1000
-            self.mode_parameters["grid_power_min"] = mode_config.get_float("grid_power_min") * 1000
-            self.mode_function = self.peak_shaving
-        elif mode == "self_consumption":
-            self.mode_parameters["grid_target"] = mode_config.get_float("grid_target") * 1000
-            self.mode_function = self.self_consumption
         elif mode == "self_peak_shaving":
-            self.mode_parameters["grid_power_max"] = mode_config.get_float("grid_power_max") * 1000
-            self.mode_parameters["charge_power_parameter"] = mode_config.get_float("charge_power_parameter") * 1000
-            self.mode_parameters["reserve_soc"] = mode_config.get_float("reserve_soc")
-            self.mode_parameters["grid_target"] = mode_config.get_float("grid_target") * 1000
-            self.mode_parameters["reserve_charge_power"] = mode_config.get_float("reserve_charge_power") * 1000
+            self._mode_parameters["grid_power_target"] = mode_section.get_float("grid_power_target", default=0.0) * 1000
+            self._mode_parameters["grid_power_max"] = mode_section.get_float("grid_power_max") * 1000
+            self._mode_parameters["grid_power_min"] = mode_section.get_float("grid_power_min") * 1000
+            self._mode_parameters["soc_reserve"] = mode_section.get_float("soc_reserve")
+            self._predict_charge_power = self._predict_self_peak_shaving
 
-            #TODO:
-            self.mode_parameters["grip_power_min"] = mode_config.get_float("grip_power_min") * 1000
-
-            self.mode_function = self.self_peak_shaving
+        elif mode == "peak_shaving":
+            self._mode_parameters["grid_power_max"] = mode_section.get_float("grid_power_max") * 1000
+            self._mode_parameters["grid_power_min"] = mode_section.get_float("grid_power_min") * 1000
+            self._predict_charge_power = self._predict_peak_shaving
 
         def add_channel(constant: Constant, **custom) -> None:
             channel = constant.to_dict()
@@ -91,11 +93,11 @@ class ElectricalEnergyStorage(Component):
         return capacity / self.capacity * 100
 
     # noinspection PyUnresolvedReferences
-    def infer_soc(self, data: pd.DataFrame, soc: float = 50.0) -> pd.DataFrame:
+    def predict(self, data: pd.DataFrame, soc: float = 50.0) -> pd.DataFrame:
         from penguin.system import System
 
         if System.POWER_EL not in data.columns:
-            raise ValueError("Unable to infer battery storage state of charge without import/export power")
+            raise ValueError("Unable to predict battery storage state of charge without import/export power")
 
         columns = [self.STATE_OF_CHARGE, self.POWER_CHARGE]
 
@@ -107,8 +109,8 @@ class ElectricalEnergyStorage(Component):
             if prior is not None:
                 grid_power = row[System.POWER_EL]
                 hours = (index - prior).total_seconds() / 3600.0
-                charge_power = self.mode_function(grid_power, soc, hours)
 
+                charge_power = self._predict_charge_power(hours, soc, grid_power)
                 if charge_power != 0:
                     soc += self.energy_to_percent(charge_power / 1000.0 * hours)
 
@@ -116,117 +118,48 @@ class ElectricalEnergyStorage(Component):
             results.append([soc, charge_power])
         return pd.DataFrame(results, index=data.index, columns=columns)
 
-    def get_max_charge_discharge_power(self, soc: float, soc_max: float, soc_min: float, hours: float) -> tuple:
-        """
-        Calculate the maximum charge and discharge power based on SoC.
-        """
-        charge_power_max = min(self.charge_power_max, self.percent_to_energy(soc_max - soc) * 1000.0 / hours)
-        charge_power_min = max(-self.discharge_power_max, self.percent_to_energy(soc_min - soc) * 1000.0 / hours)
+    def _predict_self_consumption(self, hours: float, soc: float, grid_power: float) -> float:
+        grid_power_target = self._mode_parameters["grid_power_target"]
 
-        return charge_power_max, charge_power_min
+        # Calculate the charge power based on the grid power and the grid power min
+        if grid_power > grid_power_target:
+            charge_power = grid_power_target - grid_power
+        elif grid_power < 0:
+            charge_power = abs(grid_power)
+        else:
+            charge_power = 0.0
+        return self._limit_charge_power(hours, soc, charge_power)
 
-    def peak_shaving(self, grid_power: float, soc: float, hours: float) -> float:
-        """
-        Apply the peak shaving strategy to the given grid power and state of charge.
-        """
+    def _predict_self_peak_shaving(self, hours: float, soc: float, grid_power: float) -> float:
+        soc_reserve = self._mode_parameters["soc_reserve"]
 
-        grid_power_max = self.mode_parameters["grid_power_max"]
-        grid_power_min = self.mode_parameters["grid_power_min"]
+        # First, check if the grid power is above grid_power_max
+        if soc < soc_reserve or grid_power > self._mode_parameters["grid_power_max"]:
+            charge_power = self._predict_peak_shaving(hours, soc, grid_power)
+            charge_power = min(charge_power, self.percent_to_energy(soc_reserve - soc) * 1000.0 / hours)
+            return charge_power
+        return self._predict_self_consumption(hours, soc, grid_power)
 
-        charge_power = 0
+    def _predict_peak_shaving(self, hours: float, soc: float, grid_power: float) -> float:
+        grid_power_max = self._mode_parameters["grid_power_max"]
+        grid_power_min = self._mode_parameters["grid_power_min"]
+
         # Calculate the charge power based on the grid power and the grid power limits
         if grid_power > grid_power_max:
-            charge_power = grid_power_max - grid_power
+            return self._limit_charge_power(hours, soc, grid_power_max - grid_power, hard_max=True)
         elif grid_power < grid_power_min:
-            charge_power = grid_power_min - grid_power
-
-        # Calculate the maximum remaining charge and discharge power
-        charge_power_max, charge_power_min = \
-            self.get_max_charge_discharge_power(soc, 100, 0, hours)
-
-        # Limit the charge power to the maximum allowed charge and discharge power
-        charge_power = min(charge_power, charge_power_max)
-        charge_power = max(charge_power, charge_power_min)
-
-        return charge_power
-
-    def self_consumption(self, grid_power: float, soc: float, hours: float) -> float:
-        """
-        Apply the self consumption strategy to the given grid power and state of charge.
-        """
-        grid_target = self.mode_parameters["grid_target"]
-
-        charge_power = 0
-        # Calculate the charge power based on the grid power and the grid setpoint
-        if grid_power > grid_target:
-            charge_power = grid_target - grid_power
-        elif grid_power < grid_target:
-            charge_power = grid_target - grid_power
-
-        # Calculate the maximum remaining charge and discharge power
-        charge_power_max, charge_power_min = \
-            self.get_max_charge_discharge_power(soc, 100, 0, hours)
-
-        # Limit the charge power to the maximum allowed charge and discharge power
-        charge_power = min(charge_power, charge_power_max)
-        charge_power = max(charge_power, charge_power_min)
-
-        return charge_power
-
-    def self_peak_shaving(self, grid_power: float, soc: float, hours: float) -> float:
-        """
-        Apply the self peak shaving strategy to the given grid power and state of charge.
-        """
-
-        """
-        die Entscheidung bis wie viel Netzbezug geladen wird sollte imho nicht grid_min sein sondern max(0, min(grid_min, grid_power)), solange soc > reserve_soc ist.
-        Mit Parametern kommst du da nciht weit, da du ja immernoch charge_enable_power ~= 100 kW brauchst, wenn soc < reserve_soc
-        """
-        mode_parameters = self.mode_parameters
-
-        grid_power_max = mode_parameters["grid_power_max"]
-        charge_power_parameter = mode_parameters["charge_power_parameter"]
-        reserve_soc = mode_parameters["reserve_soc"]
-        grid_target = mode_parameters["grid_target"]
-        reserve_charge_power = mode_parameters["reserve_charge_power"]
-        grip_power_min = mode_parameters["grip_power_min"]
-
-        charge_power = 0
-        # First check if the grid power is above  grid_power_max
-        if grid_power > grid_power_max:
-            charge_power = grid_power_max - grid_power
-
-        # Second check if SoC is below the reserve SoC
-        elif soc < reserve_soc:
-            # max charge power to reach grid_power_max
-            charge_power = grid_power_max - grid_power
-
-            # available power till grid_power_max
-            power_max_diff = grid_power_max - grid_power
-
-            # Limit the charge power to the maximum allowed charge
-            charge_power = min(charge_power, reserve_charge_power)
-            charge_power = min(charge_power, power_max_diff)
-
-        # Else use self consumption
+            return self._limit_charge_power(hours, soc, grid_power_min - grid_power)
         else:
-            if grid_power > grid_target:
-                charge_power = grid_target - grid_power
-            elif grid_power < grid_target:
-                charge_power = grid_target - grid_power
+            return 0.0
 
-            # TODO: new strategy: AdMin?
-            # only charge if grid_power is below grid_power_min
-            charge_power = max(0, min(grip_power_min, grid_power))
-
-            # Limit the charge power to the maximum allowed charge and discharge power
-            charge_power = min(charge_power, charge_power_parameter)
-            charge_power = max(charge_power, -charge_power_parameter)
-
-        # Limit the charge power to the maximum allowed charge and discharge power
-        charge_power_max, charge_power_min = \
-            self.get_max_charge_discharge_power(soc, 100, 0, hours)
-        charge_power = min(charge_power, charge_power_max)
-        charge_power = max(charge_power, charge_power_min)
-
+    def _limit_charge_power(self, hours: float, soc: float, charge_power: float, hard_max: bool = False) -> float:
+        charge_power_max = self.power_max
+        if not hard_max:
+            charge_power_max = min(charge_power_max, self._mode_parameters["charge_power_max"])
+        if charge_power > 0:
+            charge_power_max = min(charge_power_max, self.percent_to_energy(self.soc_max - soc) * 1000.0 / hours)
+            charge_power = min(charge_power, charge_power_max)
+        elif charge_power < 0:
+            charge_power_max = max(-charge_power_max, self.percent_to_energy(self.soc_min - soc) * 1000.0 / hours)
+            charge_power = max(charge_power, charge_power_max)
         return charge_power
