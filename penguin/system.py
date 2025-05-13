677b3ab97e31b1862b4f65eeb9c44ca2711f2711
Adrian Minde <adrian.minde@mail.de>
2025-05-13 15:55:28 +0200

    Add system extreme weeks plots

--- penguin/system.py
+++ penguin/system.py
@@ -187,7 +187,7 @@ class System(lori.System):
                 data[System.POWER_EL] -= data[solar_column]
         return data
 
-    # noinspection PyUnusedLocal, PyUnresolvedReferences
+    # noinspection PyUnusedLocal
     def _simulate_storage(
         self,
         data: pd.DataFrame,
@@ -248,8 +248,7 @@ class System(lori.System):
             data = pd.concat([data, errors], axis="columns")
         return data
 
-    # noinspection PyUnresolvedReferences
-    def _evaluate_yield(self, results: Results, data: pd.DataFram) -> None:
+    def _evaluate_yield(self, results: Results, data: pd.DataFrame) -> None:
         if not self.components.has_type(SolarSystem) or SolarSystem.POWER not in data["predictions"].columns:
             return
 
@@ -330,7 +329,6 @@ class System(lori.System):
             dc_energy = (data[("predictions", SolarSystem.POWER_DC)] / 1000.0 * hours).sum()
             results.append(Result.from_const(SolarSystem.YIELD_ENERGY_DC, dc_energy, header="Yield"))
 
-    # noinspection PyUnresolvedReferences
     def _evaluate_storage(self, results: Results, data: pd.DataFrame) -> None:
         if not self.components.has_type(ElectricalEnergyStorage):
             return
@@ -375,26 +373,63 @@ class System(lori.System):
 
         active_power = data[("predictions", System.POWER_EL)]
         import_power = active_power.where(active_power >= 0, other=0)
-        import_energy = (import_power / 1000 * hours).sum()
+        import_energy = (import_power / 1000 * hours)
         export_power = active_power.where(active_power <= 0, other=0).abs()
-        export_energy = (export_power / 1000 * hours).sum()
+        export_energy = (export_power / 1000 * hours)
 
         results.add("grid_export_max", "Export Peak [W]", export_power.max(), header="Grid", order=10)
         results.add("grid_import_max", "Import Peak [W]", import_power.max(), header="Grid", order=10)
-        results.add("grid_export", "Export [kWh]", export_energy, header="Grid", order=10)
-        results.add("grid_import", "Import [kWh]", import_energy, header="Grid", order=10)
+        results.add("grid_export", "Export [kWh]", export_energy.sum(), header="Grid", order=10)
+        results.add("grid_import", "Import [kWh]", import_energy.sum(), header="Grid", order=10)
 
         if SolarSystem.POWER in data["predictions"].columns:
             solar_power = data[("predictions", SolarSystem.POWER)]
-            solar_energy = (solar_power / 1000 * hours).sum()
+            solar_energy = (solar_power / 1000 * hours)
 
             cons_energy = import_energy + solar_energy - export_energy
-            cons_self = ((solar_power - export_power) / 1000 * hours).sum() / solar_energy * 100
-            autarky = (1 - (import_energy / cons_energy)) * 100
+            cons_self = (solar_energy - export_energy).sum() / solar_energy.sum() * 100
+            suff_self = (1 - (import_energy.sum() / cons_energy)) * 100
 
-            results.add("consumption", "Energy [kWh]", cons_energy, header="Load", order=10)
+            results.add("consumption", "Energy [kWh]", cons_energy.sum(), header="Load", order=10)
             results.add("self_consumption", "Self-Consumption [%]", cons_self, header="Consumption", order=10)
-            results.add("autarky", "Autarky [%]", autarky, header="Consumption", order=10)
+            results.add("self_sufficiency", "Self-Sufficiency [%]", suff_self, header="Consumption", order=10)
+
+        try:
+            # import_peak_energy = import_energy[import_power >= import_power.max()]
+            # import_peak_time = import_peak_energy.index.time.min()
+            # import_peak_date = import_peak_energy[import_peak_energy.index.time == import_peak_time].index.date[0]
+            # self._plot_system(
+            #     data["predictions"][data.index.date == import_peak_date],
+            #     title="Day with earliest Peak",
+            #     file=str(results.dirs.tmp.joinpath("power_peak.png")),
+            #     width=16,
+            # )
+
+            import_week_energy = import_energy.groupby(import_energy.index.isocalendar().week).sum()
+            import_week_energy_max = import_week_energy[import_week_energy == import_week_energy.max()].index[0]
+            self._plot_system(
+                data["predictions"][data.index.isocalendar().week == import_week_energy_max],
+                title="Week with highest Grid Import",
+                file=str(results.dirs.tmp.joinpath("week_max_import.png")),
+            )
+
+            if self.components.has_type(SolarSystem):
+                solar_power = data[("predictions", SolarSystem.POWER)]
+                solar_energy = (solar_power / 1000 * hours)
+
+                cons_self = solar_energy - export_energy
+                cons_self_week_energy = cons_self.groupby(cons_self.index.isocalendar().week).sum()
+                cons_self_week_energy_max = cons_self_week_energy[
+                    cons_self_week_energy == cons_self_week_energy.max()
+                ].index[0]
+                self._plot_system(
+                    data["predictions"][data.index.isocalendar().week == cons_self_week_energy_max],
+                    title="Week with highest Grid Import",
+                    file=str(results.dirs.tmp.joinpath("week_max_self-cons.png")),
+                )
+
+        except ImportError:
+            pass
 
     # noinspection PyMethodMayBeStatic
     def _evaluate_weather(self, results: Results, data: pd.DataFrame) -> None:
@@ -406,5 +441,155 @@ class System(lori.System):
         ghi = (data[("predictions", Weather.GHI)] / 1000.0 * hours).sum()
         dhi = (data[("predictions", Weather.DHI)] / 1000.0 * hours).sum()
 
-        results.add(Weather.GHI, f"{Weather.GHI.name} [kWh/m^2]", ghi, header="Weather")
-        results.add(Weather.DHI, f"{Weather.DHI.name} [kWh/m^2]", dhi, header="Weather")
+        results.add(Weather.GHI, f"{Weather.GHI.name} [kWh/m²]", ghi, header="Weather")
+        results.add(Weather.DHI, f"{Weather.DHI.name} [kWh/m²]", dhi, header="Weather")
+
+    # noinspection PyTypeChecker
+    def _plot_system(
+        self,
+        data: pd.DataFrame,
+        title: Optional[str] = None,
+        width: Optional[int] = None,
+        height: Optional[int] = None,
+        show: bool = False,
+        file: str = None,
+    ) -> None:
+        import seaborn as sns
+        import matplotlib.dates as dates
+        import matplotlib.pyplot as plt
+        from lori.io import plot
+
+        columns_power = [System.POWER_EL]
+
+        # TODO: Replace with tariff component constants
+        has_tariff = "tariff" in data.columns
+        has_solar = self.components.has_type(SolarSystem)
+        if has_solar:
+            columns_power.append(SolarSystem.POWER)
+        has_ees = self.components.has_type(ElectricalEnergyStorage)
+        if has_ees:
+            columns_power.append(ElectricalEnergyStorage.POWER_CHARGE)
+
+        data_power = deepcopy(data[columns_power])
+        data_power /= 1000
+
+        if width is None:
+            width = plot.WIDTH
+        if height is None:
+            height = plot.HEIGHT
+        figure, ax_power = plt.subplots(figsize=(width / plot.INCH, height / plot.INCH), dpi=120)
+        axes = [ax_power]
+
+        sns.lineplot(
+            data_power[System.POWER_EL],
+            linewidth=0.25,
+            color="#004f9e",
+            label="_hidden",
+            ax=axes[0],
+        )
+        if has_ees:
+            ax_soc = axes[0].twinx()
+            axes.append(ax_soc)
+
+            sns.lineplot(
+                data_power[ElectricalEnergyStorage.POWER_CHARGE],
+                linewidth=0.25,
+                color="#ff9995",
+                label="_hidden",
+                ax=ax_power,
+            )
+            sns.lineplot(
+                data[ElectricalEnergyStorage.STATE_OF_CHARGE],
+                linewidth=1,
+                color="#333333",
+                label="Battery State",
+                ax=ax_soc
+            )
+
+            data_ref = data_power[System.POWER_EL] - data_power[ElectricalEnergyStorage.POWER_CHARGE]
+            data_ref.plot.area(
+                stacked=False,
+                label="_hidden",
+                color={"_hidden": "#dddddd"},
+                linewidth=0,
+                alpha=0.75,
+                ax=ax_power,
+            )
+
+            ax_soc.set_ylim(-1, 119)
+            ax_soc.yaxis.set_label_text("State of Charge [%]")
+            if has_tariff:
+                ax_soc.legend(ncol=1, loc="upper right", bbox_to_anchor=(0.84, 1), frameon=False)
+            else:
+                ax_soc.legend(ncol=1, loc="upper right", frameon=False)
+
+        if has_solar:
+            data_power[SolarSystem.POWER].plot.area(
+                stacked=False,
+                label="PV Generation",
+                color={"PV Generation": "#ffeb9b"},
+                linewidth=0,
+                alpha=0.75,
+                ax=ax_power,
+            )
+
+        data_power[System.POWER_EL].plot.area(
+            stacked=False,
+            label="Residual Load",
+            alpha=0.25,
+            ax=ax_power,
+        )
+
+        if has_ees:
+            data_power[ElectricalEnergyStorage.POWER_CHARGE].plot.area(
+                stacked=False,
+                label="Battery Charging",
+                color={"Battery Charging": "#ff9995"},
+                alpha=0.25,
+                ax=ax_power,
+            )
+
+        if has_tariff:
+            # TODO: Replace with tariff component constants
+            tariff = data["tariff"]
+
+            ax_price = axes[0].twinx()
+            axes.append(ax_price)
+
+            sns.lineplot(tariff, linewidth=1, color="#999999", label="Dynamic Tariff", ax=ax_price)
+
+            ax_price.spines.right.set_position(("axes", 1.07))
+            ax_price.set_ylim(min(tariff.min() - 0.05), max(tariff.max()) + 0.1)
+            ax_price.yaxis.set_label_text("Price [€/kWh]")
+            ax_price.legend(ncol=1, loc="upper right", frameon=False)
+
+        ax_power.set_xlim(data_power.index[0], data_power.index[-1])
+        ax_power.set_ylim(min(data_power.min()), max(data_power.max()) + 50)
+        ax_power.xaxis.set_minor_locator(dates.HourLocator(interval=12))
+        ax_power.xaxis.set_minor_formatter(dates.DateFormatter("%H:%M", tz="Europe/Berlin"))
+        ax_power.xaxis.set_major_locator(dates.DayLocator(interval=1))
+        ax_power.xaxis.set_major_formatter(dates.DateFormatter("\n%A", tz="Europe/Berlin"))
+        ax_power.xaxis.set_label_text(
+            f"{data.index[0].strftime('%d. %B')} to "
+            f"{data.index[-1].strftime('%d. %B')}"
+        )
+        # ax_power.xaxis.label.set_visible(False)
+        ax_power.yaxis.set_label_text("Power [kW]")
+        ax_power.legend(ncol=3, loc="upper left", frameon=False)
+
+        for pos in ['right', 'top', 'bottom', 'left']:
+            for ax in axes:
+                ax.spines[pos].set_visible(False)
+
+        axes[0].grid(color="grey", linestyle="--", linewidth=0.25, alpha=0.5, axis="both")
+        axes[0].set_title(title)
+        figure.tight_layout()
+
+        if file is not None:
+            figure.savefig(file)
+        if show:
+            figure.show()
+            # figure.waitforbuttonpress()
+
+        plt.close(figure)
+        plt.clf()
