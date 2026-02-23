2f0dc3f33a21b2b6576d0d10983b1e229043604e
Adrian Minde <adrian.minde@mail.de>
2026-02-23 14:40:17 +0100

    Add watering flow to irrigation component

--- sparcs/components/agriculture/irrigation.py
+++ sparcs/components/agriculture/irrigation.py
@@ -18,7 +18,8 @@ from sparcs.components.agriculture import SoilMoisture
 class Irrigation(Component):
     TYPE = "irrigation"
 
-    STATE = Constant(bool, "state", "Irrigation state")
+    STATE = Constant(bool, "state", "Irrigation State", alias="irrigation_state")
+    FLOW = Constant(float, "flow", "Irrigation Flow", alias="irrigation_flow", unit="l/min")
 
     soil: Sequence[SoilMoisture]
 
@@ -29,3 +30,4 @@ class Irrigation(Component):
     def configure(self, configs: Configurations) -> None:
         super().configure(configs)
         self.data.add(Irrigation.STATE, aggregate="max")
+        self.data.add(Irrigation.FLOW, aggregate="sum")
